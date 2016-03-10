c411ffe48dd8ae948a7dc8279b76603412ada175
stalkert <taranenko.yura@gmail.com>
2016-03-10 15:40:08 +0200

    Перенес функцию фильтрации в фабрику

--- app/scripts/controllers/products-controller.js
+++ app/scripts/controllers/products-controller.js
@@ -6,27 +6,6 @@ angular.module('testua')
       .success(function (data) {
         listProduct.item = data;
         $rootScope.choosenId = [];
-        $rootScope.getFilterPoducts = function (product) {
-          var choosenIdLength = $rootScope.choosenId.length;
-          if (choosenIdLength === 0) {
-            return true;
-          } else {
-            for (var i = 0; i < choosenIdLength; i++) {
-              if(product.categoryId === $rootScope.choosenId[i]) {
-                return true;
-              }
-            }
-          }
-        };
+        $rootScope.getFilterPoducts = Product.getFilterPoducts;
       });
-
-    /* listProduct.getListCategoryId = function (itemProduct) {
-
-     var arrCatId = $rootScope.arrSubCat.length;
-     for (var i = 0; i < arrCatId; i++) {
-     if (itemProduct.categoryId == $rootScope.arrSubCat[i]) {
-     return itemProduct.categoryId === $rootScope.arrSubCat[i];
-     }
-     }
-     };*/
   });
--- app/scripts/factory/products-factory.js
+++ app/scripts/factory/products-factory.js
@@ -1,14 +1,26 @@
 'use strict';
 angular.module('testua')
-  .factory('Product', function ProductFactory($http) {
+  .factory('Product', function ProductFactory($http,$rootScope) {
 
     return {
       getProductsAll: function () {
         return $http.get('data/product.json');
       },
       getProductById: function () {
-       return $http.get('data/product.json');
+        return $http.get('data/product.json');
 
+      },
+      getFilterPoducts: function (product) {
+        var choosenIdLength = $rootScope.choosenId.length;
+        if (choosenIdLength === 0) {
+          return true;
+        } else {
+          for (var i = 0; i < choosenIdLength; i++) {
+            if (product.categoryId === $rootScope.choosenId[i]) {
+              return true;
+            }
+          }
+        }
       }
     };
   });
