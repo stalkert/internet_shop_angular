42e5199a9082b2426bc3a13e311c46dae7a141fe
stalkert <taranenko.yura@gmail.com>
2016-05-05 13:38:29 +0300

    попробовал директиву ng-class

--- app/scripts/controllers/product-single-controller.js
+++ app/scripts/controllers/product-single-controller.js
@@ -2,6 +2,10 @@
 angular.module('testua')
   .controller('SingleProduct',['$route','Product', function ($route, Product) {
     var one = this;
+    one.borderClass = false;
+    one.setGreenBorder =function(){
+      one.borderClass = !one.borderClass;
+    };
     var productId = parseInt($route.current.params.productId);
     Product.getProductsAll().success(function (data) {
       one.product = data;
